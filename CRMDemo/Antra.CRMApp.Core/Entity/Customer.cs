5c1c81f94b5684c9ac4d8850c6ac1d08621bc2d9
megahigh5 <102623149+megahigh5@users.noreply.github.com>
2022-06-08 10:49:38 -0500

    Add files via upload

---
+++ CRMDemo/Antra.CRMApp.Core/Entity/Customer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+
+namespace Antra.CRMApp.Core.Entity
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Customer Name is required")]
+        [Column(TypeName = "nvarchar(30)")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [Column(TypeName = "nvarchar(30)")]
+        public string Title { get; set; }
+
+
+        [Required(ErrorMessage = "Address is required")]
+        [Column(TypeName = "nvarchar(60)")]
+        public string Address { get; set; }
+
+
+        [Required(ErrorMessage = "City is required")]
+        [Column(TypeName = "nvarchar(15)")]
+        public string City { get; set; }
+
+        public int RegionId { get; set; }
+
+        [Required(ErrorMessage = "PostalCode is required")]
+        [Column(TypeName = "nvarchar(10)")]
+        public string PostalCode { get; set; }
+
+
+        [Required(ErrorMessage = "Country is required")]
+        [Column(TypeName = "nvarchar(15)")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        [Column(TypeName = "nvarchar(24)")]
+        public string Phone { get; set; }
+
+    }
+}
