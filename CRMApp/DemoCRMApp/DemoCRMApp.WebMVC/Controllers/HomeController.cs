9399faf11e84e11d15f7d5e90f4fcd16728b9bfb
megahigh5 <kenaik_908@hotmail.com>
2022-06-09 09:25:29 -0500

    Initial

---
+++ CRMApp/DemoCRMApp/DemoCRMApp.WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using DemoCRMApp.WebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DemoCRMApp.WebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CRMApp/DemoCRMApp/DemoCRMApp.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoCRMApp.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
